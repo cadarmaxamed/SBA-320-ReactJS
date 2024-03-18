f4bb6c3490ec80469fe382dfa8b915d534859f48
Cadar Maxamed <cadarmaxamed@Cadars-MacBook-Air.local>
2024-03-18 09:37:15 -0700

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css';
 import Weather from './components/Weather';
-import Search from './components/Search';
+import Search from './components/Search.jsx';
 
 function App() {
-  return(
-  <div classname= "App">
+  return
+  <div className= "App">
     <Weather/>
     <Search/>
 
 
   </div>
-  );
+  
 }
 export default App;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,11 @@
+import Search from '../components/Search';
+
+
+export default function Weather() {
+    return
+       <div>
+        <Search/>
+        Weather
+       </div>
+    
+}
