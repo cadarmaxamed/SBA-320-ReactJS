66d7828d6f711bf26128a93ce52acde6effb1b93
Cadar Maxamed <cadarmaxamed@Cadars-MacBook-Air.local>
2024-03-18 11:21:16 -0700

    changed title and other files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
+import react from 'react';
+//import { useState, useEffect} from 'react;
 import './App.css';
-import Weather from './components/Weather';
+import Weather from './components/Weather.jsx';
 import Search from './components/Search.jsx';
 
 function App() {
-  return
-  <div className= "App">
+  return(
     <Weather/>
-    <Search/>
-
-
-  </div>
+    
+  );
   
-}
+};
 export default App;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,11 +1,58 @@
-import Search from '../components/Search';
+//import React, { useEffect, useState } from 'react';
+import react from 'react';
 
+//import Search from '../components/Search';
 
-export default function Weather() {
-    return
-       <div>
-        <Search/>
-        Weather
-       </div>
+// function Weather () {
+//     return( <h1>Work Please</h1>);
     
+// };
+// export default Weather;
+export default function Weather() {
+    const [Search, setSearch] = useState("");
+    const [loading, setLoading ]= useState(false);
+    const [weatherData, setWeatherData] = useState(null);
+
+
+
+ //getting the API to fetch the weather data
+ async function fetchWeatherData(param) {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q={search}&appid={395b22df51a4c264230e093160dc365d}"
+      );
+
+      const data = await response.json();
+      if (data) {
+        setWeatherData(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  async function handleSearch() {
+    fetchWeatherData(Search);
+  }
+  function getWeatherData() {
+    return new Date().toLocaleDateString('en-us', {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+  });
+  }
+
+  useEffect(() => {
+    fetchWeatherData("Dubai");
+  }, []);
+
+  console.log(weatherData);
+
+  return (
+    <h1>React APP</h1>
+  )
 }
