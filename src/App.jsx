a8265a9565717a5092edb164009b7b2fa5af81d8
Cadar Maxamed <cadarmaxamed@Cadars-MacBook-Air.local>
2024-03-18 01:14:26 -0700

    importing weather and search files to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Weather from './components/weather';
+import Search from './components/search';
 
 function App() {
   return(
   <div classname= "App">
+    <weather/>
+
 
   </div>
   );
 }
+export default App;
